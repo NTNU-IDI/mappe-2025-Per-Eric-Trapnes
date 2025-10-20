1a234473f42c837c1c90f57af97fc17b758d98b7
Per Eric Trapnes <peretr@stud.ntnu.no>
2025-10-20 18:19:12 +0200

    First page UI

--- src/main/java/edu/ntnu/idi/idatt/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/Main.java
@@ -1,5 +1,22 @@
-package java.edu.ntnu.idi.idatt;
+package edu.ntnu.idi.idatt;
+
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Welcome to your digital diary!");
+            System.out.println("At any point you can type 'exit' to close the program");
+            System.out.print("Have I seen you before? (Do you have an account. Y/N): ");
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("exit")) {
+                break;
+            }
+        }
+        scanner.close();
+        System.out.println("Exiting...");
+    }
 }
