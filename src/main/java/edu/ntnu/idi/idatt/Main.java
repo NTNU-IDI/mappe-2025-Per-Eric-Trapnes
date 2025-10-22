2d6a8ed9d48eff1d9f5d5429243be2b96ee9dbe1
Per Eric Trapnes <peretr@stud.ntnu.no>
2025-10-22 19:53:48 +0200

    Page update
    added back as a keyword for going back to a previous page

--- src/main/java/edu/ntnu/idi/idatt/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/Main.java
@@ -8,8 +8,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    // Checks if the string is 'exit' and closes the program, or returns the command
-    // if its not
+    // Handels user input, checks for keyword 'exit'
     public static String exitCheck(String command) {
         if (command.equalsIgnoreCase("exit")) {
             System.out.println("Exiting...");
@@ -18,53 +17,99 @@ public class Main {
         return command;
     }
 
+    public static int startScreen(Scanner scanner) {
+        System.out.println("Welcome to your digital diary!");
+        System.out.println("At any point you can type 'exit' to close the program");
+
+        return 1;
+    }
+
+    public static int loginScreen(Scanner scanner) {
+        System.out.print("Have I seen you before? (Do you have an account. Yes/No): ");
+        String userInput = exitCheck(scanner.nextLine());
+
+        if (userInput.equalsIgnoreCase("back")) {
+            return -1;
+        }
+
+        // If has account
+        if (userInput.equalsIgnoreCase("Yes")) {
+            // Checks the String the user gives and compares with file
+            System.out.println("Welcome back!\n");
+            try (BufferedReader bufferedReader = new BufferedReader(
+                    new FileReader("src\\main\\java\\edu\\ntnu\\idi\\idatt\\Username.txt"))) {
+                String line;
+                System.out.print("Username: ");
+
+                String username = exitCheck(scanner.nextLine());
+                if (userInput.equalsIgnoreCase("back")) {
+                    return -1;
+                }
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    if (line.contains(username)) {
+                        System.out.println("You got a account!");
+                        return 1;
+                    } else {
+                        System.out.println("Sorry, that account does not exist.");
+                        return 0;
+                    }
+                }
+            } catch (IOException errorMessage) {
+                System.err.println("Error reading file: " + errorMessage.getMessage());
+            }
+
+            // If does not have account
+        } else if (userInput.equalsIgnoreCase("No")) {
+            // Writes the username given to the 'Username.txt' file.
+            // OBS can only store 1 username with currect implimentation
+            System.out.println("Make an account\n");
+            System.out.print("Username: ");
+
+            String username = exitCheck(scanner.nextLine());
+            if (username.equalsIgnoreCase("back")) {
+                return -1;
+            }
+
+            try (BufferedWriter bufferedWriter = new BufferedWriter(
+                    new FileWriter("src\\main\\java\\edu\\ntnu\\idi\\idatt\\Username.txt"))) {
+                bufferedWriter.write(username);
+                System.out.println("Saved successfully!");
+                return -1;
+
+            } catch (IOException errorMessage) {
+                System.err.println("An error occurred while writing to the file: " + errorMessage.getMessage());
+            }
+        }
+        return 0;
+    }
+
+    // --- Start of program ---
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int screenNumber = 0;
 
         while (true) {
-            // --- Start of program ---
-            System.out.println("Welcome to your digital diary!");
-            System.out.println("At any point you can type 'exit' to close the program");
-            System.out.print("Have I seen you before? (Do you have an account. Yes/No): ");
-            String userInput = exitCheck(scanner.nextLine());
+            switch (screenNumber) {
 
-            // If has account
-            if (userInput.equalsIgnoreCase("Yes")) {
-                // Checks the String the user gives and compares with file
-                System.out.println("Welcome back!\n");
-                try (BufferedReader bufferedReader = new BufferedReader(
-                        new FileReader("src\\main\\java\\edu\\ntnu\\idi\\idatt\\Username.txt"))) {
-                    String line;
-                    System.out.print("Username: ");
-                    String username = exitCheck(scanner.nextLine());
+                case -1:
+                    screenNumber = 0;
+                    break;
+                case 0:
+                    screenNumber += startScreen(scanner);
+                    break;
+                case 1:
+                    screenNumber += loginScreen(scanner);
+                    break;
+                case 2:
+                    System.out.println("You logged inn!");
+                    System.exit(0);
+                    break;
+                default:
+                    break;
 
-                    while ((line = bufferedReader.readLine()) != null) {
-                        if (line.contains(username)) {
-                            System.out.println("You got a account!");
-                        } else {
-                            System.out.println("Sorry, that account does not exist.");
-                        }
-                    }
-                } catch (IOException errorMessage) {
-                    System.err.println("Error reading file: " + errorMessage.getMessage());
-                }
-
-                // If does not have account
-            } else if (userInput.equalsIgnoreCase("No")) {
-                // Writes the username given to the 'Username.txt' file.
-                // OBS can only store 1 username with currect implimentation
-                System.out.println("Make an account\n");
-                System.out.print("Username: ");
-                String username = exitCheck(scanner.nextLine());
-
-                try (BufferedWriter bufferedWriter = new BufferedWriter(
-                        new FileWriter("src\\main\\java\\edu\\ntnu\\idi\\idatt\\Username.txt"))) {
-                    bufferedWriter.write(username);
-                    System.out.println("Saved successfully!");
-                } catch (IOException errorMessage) {
-                    System.err.println("An error occurred while writing to the file: " + errorMessage.getMessage());
-                }
             }
+
         }
 
     }
